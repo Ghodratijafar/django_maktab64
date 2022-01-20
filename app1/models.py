4ae7e5fa21f6fb4e3aea3c64418263b0e01aa763
Ghodratijafar <jamalgh1376.jg@gmail.com>
2022-01-20 14:49:59 +0330

    create table Car in database

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Car(models.Model):
+    brand: models.CharField(max_length=30, null=False, default='Saipa')
+    color = models.CharField(max_length=3, choices=[
+        ('RED', 'red'),
+        ('BLU', 'blue'),
+        ('GRE', 'green'),
+        ('BLK', 'black'),
+    ], null=True,default=None)
+    acceleration = models.IntegerField()
+    create_timestamp = models.DateTimeField(auto_now_add=True)
